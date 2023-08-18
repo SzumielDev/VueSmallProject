e0b6a76548cf7570082dab4644a89f54cdba82b5
SzumielDev <107126687+SzumielDev@users.noreply.github.com>
2023-08-18 13:58:26 +0200

    Updated Contact Component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,12 +1,24 @@
 <script setup lang="ts">
+import { NGradientText } from 'naive-ui';
 
 </script>
 <template>
     <div>
-        <header>
-            <h1>Contact</h1>
-        </header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center pt-5 pb-2">
+                    <n-gradient-text gradient="linear-gradient(90deg, red 0%, green 50%, blue 100%)">
+                        Check ouy my linkedin profile 
+                    </n-gradient-text>
+                    
+                </div>
+                <div class="col-12 text-center pb-5 pt-2">
+                    <a class="text-decoration-none" href="https://www.linkedin.com/in/marcin-szumiel/">LinkendIn</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
